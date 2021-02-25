15eba2ba368fbbf2be4205b7e77256d0a74e2bf4
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-02-25 08:53:20 +0200

    museums

--- app/Http/Controllers/Admin/ExhibitController.php
+++ app/Http/Controllers/Admin/ExhibitController.php
@@ -9,6 +9,7 @@ use App\Models\Exhibit;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class ExhibitController extends Controller
 {
