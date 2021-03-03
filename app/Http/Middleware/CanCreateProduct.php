3025a4fc74827e16eaf3e8b3c824aaefbca3d452
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-03 16:27:12 +0200

    bug fix

--- app/Http/Middleware/CanCreateProduct.php
+++ app/Http/Middleware/CanCreateProduct.php
@@ -18,10 +18,10 @@ class CanCreateProduct
     public function handle($request, Closure $next)
     {
         if(Auth::user()->shop->products->count() >= app('settings')->ads_per_user &&
-            (!Auth::user()->premium || Auth::user()->premium < Carbon::now()->toDate())
+            (!Auth::user()->premium || Auth::user()->premium < now())
             )
         {
-            return redirect()->route('board.products.index')->with('warning', "You can't create product");
+            return redirect()->route('board.products.index')->with('message', "You can't create product");
         }
         return $next($request);
     }
