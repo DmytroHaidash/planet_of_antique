2458f2492cad2de8bf9d9bec62b1bf05df5f078e
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-06 15:13:58 +0200

    bug fix

--- app/Http/Controllers/Board/ProductsController.php
+++ app/Http/Controllers/Board/ProductsController.php
@@ -71,8 +71,9 @@ class ProductsController extends Controller
     public function update(ProductSavingRequest $request, Product $product): RedirectResponse
     {
         $published = $request->has('is_published');
-        if (Auth::user()->shop->products->count() >= app('settings')->ads_per_user || !Auth::user()->premium ||
+        if (Auth::user()->shop->products()->where('is_published', 1)->count() >= app('settings')->ads_per_user || !Auth::user()->premium ||
             Auth::user()->premium < now()) {
+            dd($request->all());
             $published = false;
         }
         $product->update([
