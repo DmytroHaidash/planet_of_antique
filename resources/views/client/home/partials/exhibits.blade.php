b6f259e89f06adb9fa6c848a741713b6284969f2
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-16 16:40:17 +0200

    bug fix

--- resources/views/client/home/partials/exhibits.blade.php
+++ resources/views/client/home/partials/exhibits.blade.php
@@ -8,7 +8,7 @@
         <div class="section-exhibit__slider swiper-container hidden xl:flex">
             <div class="swiper-wrapper">
                 @foreach($exhibits as $item)
-                    <a href="{{ route('client.exhibits.show', $item) }}" class="section-exhibit__slide swiper-slide slider-2">
+                    <a href="{{ route('client.exhibits.show', $item) }}" class="section-exhibit__slide swiper-slide slider-3">
                         <img src="{{$item->firstImage}}" alt="slide image">
                     </a>
                 @endforeach
