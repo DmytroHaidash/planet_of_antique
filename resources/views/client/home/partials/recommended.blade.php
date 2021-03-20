f3a66d6924aca64e0eba2e232df81962c6f55e95
Dmytro Haidash <dmyto.haidash@gmail.com>
2021-03-20 11:21:16 +0200

    bug fix

--- resources/views/client/home/partials/recommended.blade.php
+++ resources/views/client/home/partials/recommended.blade.php
@@ -11,7 +11,7 @@
                 @foreach($recommended as $item)
                     <a href="{{ route('client.catalog.show', $item) }}" class="section-recommended__slide swiper-slide slider-2">
                         <img src="{{$item->firstImage}}" alt="slide image" class="image">
-                        <span class="popular-item__link-2">{{ $item->title }}</span>
+                        <span class="popular-item__link-3">{{ $item->title }}</span>
                     </a>
                 @endforeach
             </div>
